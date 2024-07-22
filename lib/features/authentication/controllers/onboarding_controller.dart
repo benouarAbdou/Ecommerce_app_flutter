6fa279dc41398a2fad05da1aafe6088e3ce1b93f
benouarAbdou <benouarabdelouahab@gmail.com>
2024-07-22 15:18:02 +0200

    onboarding backend

--- lib/features/authentication/controllers/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/features/authentication/screens/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class OnBoardingController extends GetxController {
   static OnBoardingController get instance => Get.find();
@@ -18,6 +19,8 @@ class OnBoardingController extends GetxController {
   void nextPage(index) {
     if (index == 2) {
       Get.offAll(const LoginScreen());
+      final storage = GetStorage();
+      storage.write('isFirstTime', false);
     } else {
       int page = index + 1;
       pageController.jumpToPage(page);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
+import 'package:ecommerce/data/repositories/auth/auth_repo.dart';
 import 'package:ecommerce/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:ecommerce/firebase_options.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:ecommerce/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  //widgets binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  //init local storage
+  await GetStorage.init();
+
+  //await native
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  //init firbase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }
 
@@ -16,7 +37,14 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: EcommerceTheme.lightTheme,
       darkTheme: EcommerceTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      home: const Scaffold(
+        backgroundColor: TColors.primary,
+        body: Center(
+          child: CircularProgressIndicator(
+            color: TColors.white,
+          ),
+        ),
+      ),
     );
   }
 }
